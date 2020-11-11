7365b860cfd4a5d42e9987d8323f68c7691f025a
Евгений Радченко <evgeni.radchenko2000@gmail.com>
2020-11-12 00:09:12 +0300

    Revert "Ввела изменения в программе"
    This reverts commit 5efc2a071b2995d09bdb88522d8c367cc449e494.

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp69
             {
                 for (int j = 0; j < y; j++)
                 {
-                    mas[i, j] = random.Next(1, 5);
+                    mas[i, j] = random.Next(1, 10);
                 }
             }
             Console.WriteLine("Вывод массива случайными числами");
